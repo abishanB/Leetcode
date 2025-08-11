98a3a1dc59dd2d856e37878aac70f14426bdc967
abishanB <abishan.bhavan@gmail.com>
2025-08-10 20:20:53 -0400

    solutions

--- Neetcode/Backtracking/40-CombinationSum2.py
+++ Neetcode/Backtracking/40-CombinationSum2.py
@@ -9,17 +9,26 @@ class Solution:
     def dfs(i, curr, total):
       if total == target:
         res.append(curr[:])
+
         return
       if i >= len(candidates) or total > target:
         return
 
       curr.append(candidates[i])
       dfs(i + 1, curr, total + candidates[i])
-
       curr.pop()
+
+      i += 1
+
+      while i < len(candidates) and candidates[i] == candidates[i - 1]:
+        i += 1
+
+      dfs(i, curr, total)
+
     dfs(0, [], 0)
     return res
 
 
 Solution = Solution()
-print(Solution.combinationSum2([10, 1, 2, 7, 6, 1, 5], 7))
+print(Solution.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
+print(Solution.combinationSum2([2, 5, 2, 1, 2], 5))
