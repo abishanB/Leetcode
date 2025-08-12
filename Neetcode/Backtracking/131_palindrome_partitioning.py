8d4737af22ece2a7626118b38761cf1a1892c9cb
abishanB <abishan.bhavan@gmail.com>
2025-08-12 17:27:38 -0400

    solutions

--- Neetcode/Backtracking/131_palindrome_partitioning.py
+++ Neetcode/Backtracking/131_palindrome_partitioning.py
@@ -2,31 +2,30 @@ from typing import List
 
 
 class Solution:
-  def isPali(self, s, l, r):
+  def isPalindrome(self, s, l, r):
     while l < r:
       if s[l] != s[r]:
         return False
       l += 1
       r -= 1
-
     return True
 
   def partition(self, s: str) -> List[List[str]]:
     res = []
     part = []
 
-    def dfs(i):
+    def backtrack(i):
       if i >= len(s):
         res.append(part.copy())
         return
 
       for j in range(i, len(s)):
-        if self.isPali(s, i, j):
+        if self.isPalindrome(s, i, j):
           part.append(s[i:j + 1])
-          dfs(j + 1)
+          backtrack(j + 1)
           part.pop()
 
-    dfs(0)
+    backtrack(0)
     return res
 
 
