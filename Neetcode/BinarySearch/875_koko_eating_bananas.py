38f0d453e3da41c9e834eb4a4e843b713ffe370d
abishanB <abishan.bhavan@gmail.com>
2025-08-30 23:38:45 -0400

    875

--- Neetcode/BinarySearch/875_koko_eating_bananas.py
+++ Neetcode/BinarySearch/875_koko_eating_bananas.py
@@ -1,6 +1,27 @@
 from typing import List
+import math
 
 
 class Solution:
   def minEatingSpeed(self, piles: List[int], h: int) -> int:
-    pass
+    l = 1
+    r = max(piles)
+
+    res = r
+    while l <= r:
+      k = (l + r) // 2
+
+      hours = 0
+      for p in piles:
+        hours += math.ceil(p / k)
+
+      if hours <= h:
+        res = min(res, k)
+        r = k - 1
+      else:
+        l = k + 1
+    return res
+
+
+Solution = Solution()
+print(Solution.minEatingSpeed([3, 6, 7, 11], 8))
