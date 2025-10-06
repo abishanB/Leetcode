7b18c660b95170bc6be03d73d75e07345ac71c23
abishanB <abishan.bhavan@gmail.com>
2025-10-06 12:29:23 -0400

    678

---
+++ Neetcode/Greedy/678_valid_parenthesis_string.py
@@ -0,0 +1,28 @@
+class Solution:
+  def checkValidString(self, s: str) -> bool:
+
+    def dfs(open1, open2, open3, i):
+      if open1 < 0:
+        open1 = -1000
+      if open2 < 0:
+        open2 = -1000
+      if open3 < 0:
+        open3 = -1000
+      if open1 < 0 and open2 < 0 and open3 < 0:
+        return False
+      if i == len(s):
+        if open1 == 0 or open2 == 0 or open3 == 0:
+          return True
+        return False
+      if s[i] == "(":
+        return dfs(open1 + 1, open2 + 1, open3 + 1, i + 1)
+      if s[i] == ")":
+        return dfs(open1 - 1, open2 - 1, open3 - 1, i + 1)
+
+      return dfs(open1, open2 + 1, open3 - 1, i + 1)
+
+    return dfs(0, 0, 0, 0)
+
+
+Solution = Solution()
+print(Solution.checkValidString("*)("))
